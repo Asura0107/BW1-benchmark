aef75cc96148621b60d05ad951b4db77c45abd9c
Asura0107 <mircoorosio@gmail.com>
2023-10-17 17:55:02 +0200

    BTW1-benchmark

--- result.js
+++ result.js
@@ -1,10 +1,26 @@
-// let number = document.getElementById("number");
-// let count = 0;
-// setInterval(() => {
-//   if (count == 65) {
-//     clearInterval();
-//   } else {
-//     count += 1;
-//     number.innerHTML = count + "%";
-//   }
-// }, 20);
+// punteggio
+let userScore = document.getElementById("user-score");
+let startScreen = document.querySelector(".start-screen");
+const circle1 = document.getElementsByClassName("circle1");
+const percCorrect = document.getElementsByClassName("percCorrect");
+const percWrong = document.getElementsByClassName("percWrong");
+const correctAnswer = document.getElementsByClassName("correctAnswer");
+const wrongAnswer = document.getElementsByClassName("wrongAnswer");
+
+let questionCount;
+let scoreCount = 0;
+
+const punteggio = () => {
+  if (userSolution === quizArray[questionCount].correct) {
+    userOption.classList.add("correct");
+    scoreCount++;
+  } else {
+    userOption.classList.add("wrong");
+    // correct option
+    options.forEach((element) => {
+      if (element.innerText == quizArray[questionCount].correct) {
+        element.classList.add("correct");
+      }
+    });
+  }
+};
