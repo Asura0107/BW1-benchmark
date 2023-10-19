586408ff52637b8a27c0d5c9138a4b7444f6c993
Rafael98-stack <rafaelcasapaojr@gmail.com>
2023-10-19 12:03:06 +0200

    Commited

---
+++ Feedback/feedback.js
@@ -0,0 +1,15 @@
+const stars = document.querySelectorAll (".stars svg");
+
+stars.forEach((item, index1) => {
+    item.addEventListener("click", () => {
+        stars.forEach((star, index2) => {
+            index1 >= index2 ? star.classList.add("active") : star.classList.remove("active")
+        })
+    })
+});
+
+
+
+
+
+
