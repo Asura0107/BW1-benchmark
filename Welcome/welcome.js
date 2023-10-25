6c31c2d82844c92259e6c89676689626e00b3dab
Asura0107 <mircoorosio@gmail.com>
2023-10-25 22:50:07 +0200

    BTW1-benchmark

--- Welcome/welcome.js
+++ Welcome/welcome.js
@@ -1,9 +1,15 @@
-let checkbox = document.querySelector("input[id=check]");
+const doublecheck = function () {
+  const btn = document.getElementById("proceed-button");
+  const check = document.getElementById("check");
 
-checkbox.addEventListener("change", function () {
-  if (this.checked) {
-    console.log("Checkbox is checked..");
-  } else {
-    console.log("Checkbox is not checked..");
-  }
-});
+  btn.disabled = true;
+
+  check.addEventListener("change", function () {
+    // se la check cambia allora il mio btn da inizialmente disabled diventa enabled, altrimenti rimane disabled
+    btn.disabled = !check.checked;
+  });
+};
+
+window.onload = function () {
+  doublecheck();
+};
